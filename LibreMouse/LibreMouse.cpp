9f092e71fa8579676276e60e5f848587245d687c
HyunJun Kim <me@hyunjun.org>
2016-09-11 19:40:46 +0900

    Update library linker settings

--- LibreMouse/LibreMouse.cpp
+++ LibreMouse/LibreMouse.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <wx/wx.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
-int main()
-{
-	std::cout << "Hello, LibreMouse!" << std::endl;
+#include "LibreMouse.h"
+
+
+IMPLEMENT_APP(LibreMouse);
+
+bool LibreMouse::OnInit() {
+	wxFrame *frame = new wxFrame((wxFrame*)NULL, -1, _T("LibreMouse"));
+	frame->CreateStatusBar();
+	frame->SetStatusText(_T("Hello World"));
+	frame->Show(true);
+	SetTopWindow(frame);
+	return true;
 }
---
+++ LibreMouse/LibreMouse.h
@@ -0,0 +1,8 @@
+#pragma once
+class LibreMouse : public wxApp
+{
+public:
+	virtual bool OnInit();
+};
+
+DECLARE_APP(LibreMouse);
